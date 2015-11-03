72ec6a4023d24f8d8fb6d12c4daf6b62c934cb52
SShayashi <SShayashi0208@gmail.com>
2015-11-03 13:07:11 +0900

    templateを使って、htmlの条件分岐まで実装

--- hello/urls.py
+++ hello/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$' , views.hello_world, name = 'hello_world'),
+    url(r'^template/$' , views.hello_template , name='hello_template'),
+    url(r'^if/$' , views.hello_if , name='hello_if'),
+
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,21 @@
+from datetime import datetime
 from django.http.response import HttpResponse
+from django.shortcuts import render
+
 
 def hello_world(requet):
     return HttpResponse('Hello World')
+
+def hello_template(requet):
+    d = {
+        'hour': datetime.now().hour,
+        'message': 'Sample message',
+    }
+    return render(requet,'index.html' , d)
+
+def hello_if(requet):
+    d = {
+        'is_visible':False,
+        'empty_str': '',
+    }
+    return render(requet, 'if.html', d)

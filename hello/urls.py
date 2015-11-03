6f0ddfafd9c8470d25f92d70337b913dbef44dff
SShayashi <SShayashi0208@gmail.com>
2015-11-03 16:14:22 +0900

    for文をview上で使えるようになりました。

--- hello/urls.py
+++ hello/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$' , views.hello_world, name = 'hello_world'),
     url(r'^template/$' , views.hello_template , name='hello_template'),
     url(r'^if/$' , views.hello_if , name='hello_if'),
+    url(r'^for/$' , views.hello_for , name='hello_for'),
 
 ]
--- hello/views.py
+++ hello/views.py
@@ -3,19 +3,25 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 
-def hello_world(requet):
+def hello_world(request):
     return HttpResponse('Hello World')
 
-def hello_template(requet):
+def hello_template(request):
     d = {
         'hour': datetime.now().hour,
         'message': 'Sample message',
     }
-    return render(requet,'index.html' , d)
+    return render(request,'index.html' , d)
 
-def hello_if(requet):
+def hello_if(request):
     d = {
         'is_visible':False,
         'empty_str': '',
     }
-    return render(requet, 'if.html', d)
+    return render(request, 'if.html', d)
+
+def hello_for(request):
+    d = {
+        'objects':range(10),
+    }
+    return render(request , 'for.html',d)
